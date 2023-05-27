92a3def34ae95f67e43504b5ca66ea8e9f603b3a
马松松 <2698745548@qq.com>
2023-05-27 09:29:58 +0800

    refactor: 部分重构

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  cacheDir: './node_modules/.vite/nx-react-demo',
 
   server: {
     port: 4200,
@@ -23,21 +21,4 @@ export default defineConfig({
     }),
   ],
 
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [
-  //    viteTsConfigPaths({
-  //      root: './',
-  //    }),
-  //  ],
-  // },
-
-  test: {
-    globals: true,
-    cache: {
-      dir: './node_modules/.vitest',
-    },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-  },
 });
