e6e2f3a15b3d70617cb08ab2289348adfe784e59
马松松 <2698745548@qq.com>
2023-05-27 15:33:13 +0800

    feat: 增加一些工程化配置

---
+++ examples/react/src/App.tsx
@@ -0,0 +1,7 @@
+
+export default function App() {
+
+    return (
+        <div>这是React组件</div>
+    );
+}
---
+++ examples/vue/src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="app">这是Vue项目</div>
+</template>
